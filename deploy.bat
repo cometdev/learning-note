fbe1c275e2c25d003c0daa0d0909e0faf9ccbc8e
Park HyeongYun <phy1103@gmail.com>
2019-06-26 20:05:47 +0900

    bat 수정

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@
 cd docs/.vuepress/dist
 :: 윈도우에서 git add -A를 할 경우 개행 문자열로 인해 warning: LF will be replaced by CRLF이 발생하여 해당 CRLF 치환을 자동으로 해주도록 global 설정
 git init
-git config --global core.autocrlf true
+::git config --global core.autocrlf true
 git add -A
 git commit -m 'deploy with vuepress'
 git push -f https://github.com/cometdev/learning-note.git master:gh-pages
