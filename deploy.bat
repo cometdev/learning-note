fc40925ac4eb7c08cd01a3dcdb88cf9e6e723638
Park HyeongYun <phy1103@gmail.com>
2019-06-26 20:05:17 +0900

    build

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,9 @@
-
-::set -e
-call npm run build
-
+::call npm run build
 cd docs/.vuepress/dist
-
-REM TEST
-
 :: 윈도우에서 git add -A를 할 경우 개행 문자열로 인해 warning: LF will be replaced by CRLF이 발생하여 해당 CRLF 치환을 자동으로 해주도록 global 설정
-git config --global core.autocrlf true
-REM TEST
-
 git init
-REM TEST
-
+git config --global core.autocrlf true
 git add -A
-REM TEST
 git commit -m 'deploy with vuepress'
-REM TEST
 git push -f https://github.com/cometdev/learning-note.git master:gh-pages
-
 cd ../../../
