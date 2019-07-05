4fb49e90f73f76d0f0f97197185bc905d840aa47
unknown <hypark5@lotte.net>
2019-07-05 18:31:55 +0900

    learning-note commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Vue.js 정복 캠프 학습 노트',
-  description: '나는 오늘 무엇을 배웠는가',
+  title: "Vue.js 정복 캠프 학습 노트",
+  description: "나는 오늘 무엇을 배웠는가",
   base: "/learning-note/",
   // head: [
   //   ['link', {
@@ -11,54 +11,48 @@ module.exports = {
   themeConfig: {
     sidebar: [
       {
-        title: 'Reference',
+        title: "Reference",
+        collapsable: true,
+        children: ["/common/fav", "/common/markdown"]
+      },
+      {
+        title: "Day 1",
         collapsable: true,
         children: [
-          '/common/fav',
-          '/common/markdown',
+          "/day1/day1",
+          "/day1/day1-task",
+          "/day1/vuepress",
+          "/day1/instance"
         ]
       },
       {
-        title: 'Day 1',
-        collapsable: false,
+        title: "Day 2",
+        collapsable: true,
         children: [
-          '/day1/day1',
-          '/day1/day1-task',
-          '/day1/vuepress',
-          '/day1/instance'
+          "/day2/day2",
+          "/day2/day2-task",
+          "/day2/components",
+          "/day2/components-communication",
+          "/day2/props",
+          "/day2/event-emit"
         ]
       },
       {
-        title: 'Day 2',
-        collapsable: false,
+        title: "Day 3",
+        collapsable: true,
         children: [
-          '/day2/day2',
-          '/day2/day2-task',
-          '/day2/components',
-          '/day2/components-communication',
-          '/day2/props',
-          '/day2/event-emit'
+          "/day3/day3",
+          "/day3/day3-task",
+          "/day3/template",
+          "/day3/axios",
+          "/day3/vue-cli"
         ]
       },
       {
-        title: 'Day 3',
-        collapsable: false,
-        children: [
-          '/day3/day3',
-          '/day3/day3-task',
-          '/day3/template',
-          '/day3/axios',
-          '/day3/vue-cli'
-        ]
-      },
-      {
-        title: 'Day 4',
-        collapsable: false,
-        children: [
-          '/day4/day4',
-          '/day4/day4-task'
-        ]
+        title: "Day 4",
+        collapsable: true,
+        children: ["/day4/day4", "/day4/day4-task"]
       }
     ]
   }
-}
+};
